8faf41ae66bb131983671f4012ee4f7484e352c4
Chibi1 <chibueze.e.njoku@gmail.com>
2019-05-18 22:10:40 +1000

    testing of file reading

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,28 +1,44 @@
 package main;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "file2.txt";
+        String fileName = "file.txt";
         PrintWriter outputStream = null;
         String name = "Harry Rhodes";
         String denomination = "atheist";
         int age = 21;
         try {
             outputStream = new PrintWriter(new FileOutputStream(fileName));
-            outputStream.write("{"
-                    + "\n\t\"Name\": \"" + name + "\""
-                    + "\n\t\"Denomination\": \"" + denomination + "\""
-                    + "\n\t\"Age\": \"" + age + "\""
-                    + "\n}");
+            outputStream.write(name + ":" + denomination + ":" + age);
             System.out.println("File written without error");
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
-
         outputStream.close();
+
+        String fileName2 = "file2.txt";
+        Scanner inputStream = null;
+        String name2 = "";
+        String denomination2 = "";
+        int age2 = 0;
+        try {
+            inputStream = new Scanner(new File(fileName2));
+            inputStream.useDelimiter(":");
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+        while (inputStream.hasNextLine()) {
+            name2 = inputStream.next();
+            denomination2 = inputStream.next();
+            age2 = inputStream.nextInt();
+        }
+        System.out.println(name2 + denomination2 + age2);
     }
 }
