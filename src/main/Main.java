f71d14164a9ee6aeabb0ca2ada04bd66f4fdf4e7
Chibi1 <chibueze.e.njoku@gmail.com>
2019-05-18 20:58:32 +1000

    first running version of file output with json

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,11 +6,23 @@ import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) {
-        PrintWriter outputStream;
+        String fileName = "file2.txt";
+        PrintWriter outputStream = null;
+        String name = "Harry Rhodes";
+        String denomination = "atheist";
+        int age = 21;
         try {
-            outputStream = new PrintWriter(new FileOutputStream("file.txt"));
+            outputStream = new PrintWriter(new FileOutputStream(fileName));
+            outputStream.write("{"
+                    + "\n\t\"Name\": \"" + name + "\""
+                    + "\n\t\"Denomination\": \"" + denomination + "\""
+                    + "\n\t\"Age\": \"" + age + "\""
+                    + "\n}");
+            System.out.println("File written without error");
         } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+            System.out.println(e);
         }
+
+        outputStream.close();
     }
 }
